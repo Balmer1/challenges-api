09664bec37d30d03db1a59c2edc3e6e482babe01
Balmer1 <balmer@hotmail.com.ar>
2016-12-10 10:20:47 -0300

    working

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var url = require('url');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+index1 = function(req, res) {
+    var date = req.params.date;
+    //date = Number(date) || date;
+    date = Number(date) * 1000 || date;
+    date = new Date(date);
+    res.json({ 
+        "unix": isNaN(date) ? null : date.getTime(),
+        "natural": isNaN(date) ? null : date.toDateString()
+    });
+};
+
+index2 = function(req, res){
+  res.render('index');
+};
+
+app.engine('jade', require('jade').__express);
+app.set('views', __dirname + '/');
+app.set('view engine', 'jade');
+
+
+app.get('/:date',index1 );
+app.get('/',index2 );
+
+
+app.listen(3002, function () {
+  console.log('Example app listening on port 3002!');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
